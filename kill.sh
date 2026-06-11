72c61feabdb3d2d9883c9dd2b6ba8ec3597d0053
jixie <jixie@bytedance.com>
2026-06-12 03:12:23 +0800

    chore: remove legacy scripts and unsupported model entries

--- kill.sh
+++ kill.sh
@@ -2,5 +2,4 @@ ps -ef | grep train_lora.py | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep train_1.3b.sh | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep train_joint_img_video_lora.py | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep train_joint_img_video_lora.sh | grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | grep train_joint_img_cot_video_lora.py | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep train_joint_img_cot_video_lora.sh | grep -v grep | awk '{print $2}' | xargs kill -9
--- videox_fun/pipeline/__init__.py
+++ videox_fun/pipeline/__init__.py
@@ -1,8 +1,6 @@
 from .pipeline_wan import WanPipeline
-from .pipeline_wan2_2 import Wan2_2Pipeline
 
 WanFunPipeline = WanPipeline
-Wan2_2FunPipeline = Wan2_2Pipeline
 
 import importlib.util
 
@@ -15,7 +13,3 @@ if importlib.util.find_spec("paifuser") is not None:
     # Wan2.1
     WanFunPipeline.__call__ = sparse_reset(WanFunPipeline.__call__)
     WanPipeline.__call__ = sparse_reset(WanPipeline.__call__)
-
-    # Wan2.2
-    Wan2_2FunPipeline.__call__ = sparse_reset(Wan2_2FunPipeline.__call__)
-    Wan2_2Pipeline.__call__ = sparse_reset(Wan2_2Pipeline.__call__)
